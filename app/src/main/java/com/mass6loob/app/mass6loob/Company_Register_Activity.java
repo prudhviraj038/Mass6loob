9c9901b1ba6eaacccd4dd8df1b66477d29d81b67
prudhviraj <prudhviraj038@gmail.com>
2016-06-27 15:52:22 +0530

    hiii

--- app/src/main/java/com/mass6loob/app/mass6loob/Company_Register_Activity.java
+++ app/src/main/java/com/mass6loob/app/mass6loob/Company_Register_Activity.java
@@ -2,9 +2,14 @@ package com.mass6loob.app.mass6loob;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -21,7 +26,7 @@ public class Company_Register_Activity extends Activity {
         Settings.forceRTLIfSupported(this);
 
         setContentView(R.layout.company_register);
-        final int RESULT_LOAD_IMAGE = 1;
+
         uname = (EditText)findViewById(R.id.company_uname);
         password = (EditText)findViewById(R.id.company_pwd);
         companyname= (EditText)findViewById(R.id.company_name);
@@ -35,6 +40,7 @@ public class Company_Register_Activity extends Activity {
                 startActivityForResult(i, RESULT_LOAD_IMAGE);
             }
         });
+
         aboutcompany= (EditText)findViewById(R.id.about_company);
         submit= (LinearLayout)findViewById(R.id.company_submit_ll);
         submit.setOnClickListener(new View.OnClickListener() {
@@ -140,5 +146,44 @@ public class Company_Register_Activity extends Activity {
     // };
     // AppController.getInstance().addToRequestQueue(stringRequest);
     //  }
+    final int RESULT_LOAD_IMAGE = 1;
+    String imgDecodableString;
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.ic_upload_logo);
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+    }
 }
